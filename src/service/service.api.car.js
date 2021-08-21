b447429df947c9a5ceab88b2529da5aa94b7172a
Valery555 <sokolchenkov@gmail.com>
2021-08-21 13:53:35 +0300

    commit api

--- src/service/service.api.car.js
+++ src/service/service.api.car.js
@@ -1,5 +1,5 @@
 const saveCar = (car) => {
-    fetch('http://195.72.146.25/api/v1/cars',{
+    fetch('http://195.72.144.67/api/v1/cars',{
         method:'POST',
         body: JSON.stringify(car),
         headers:{
