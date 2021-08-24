eae70924116ec1b6f6673eddc381dd7bc27fc705
Valery555 <sokolchenkov@gmail.com>
2021-08-24 19:15:28 +0300

    commit 24/08

---
+++ src/components/users/User.js
@@ -0,0 +1,27 @@
+import Address from "../address/Address";
+import Company from "../company/Company";
+
+
+export default function Users({item: userItem}) {
+
+    let {address} = userItem;
+    let {company} = userItem;
+
+
+
+
+    return (
+        <div>
+            <h3>{userItem.id} {userItem.name} {userItem.username}</h3>
+            <p><b>email:</b>{userItem.email};</p>
+            <p><b>Phone:</b>{userItem.phone};</p>
+            <p><b>Website:</b> {userItem.website}</p>
+
+            <Address address={address}/>
+            <Company company={company}/>
+            <br/>
+
+
+        </div>
+    );
+}
