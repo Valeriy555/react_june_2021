6f1b0d53611d9344e350711015d15f9e0b1c8576
Valery555 <sokolchenkov@gmail.com>
2021-08-24 16:14:00 +0300

    commit 24.08

--- src/components/cars/Cars.js
+++ src/components/cars/Cars.js
@@ -1,32 +1,33 @@
 import {useEffect, useState} from "react";
-import {deleteCarApi,saveCars} from "../../service/service.api.car";
+import {deleteCarApi, saveCars} from "../../service/service.api.car";
 import Car from "./Car";
 import "./Cars.css"
+
 export default function Cars() {
 
     let [cars, setCars] = useState([]);
 
     useEffect(() => {
-        saveCars().then(value =>{
+        saveCars().then(value => {
             setCars([...value])
-        } );
+        });
 
 
     }, [cars]);
 
-    let deleteCar =(id)=>{
+    let deleteCar = (id) => {
         deleteCarApi(id).then(value => console.log(value));
-        let filterCarsArray = cars.filter(value=>value.id !== id);
+        let filterCarsArray = cars.filter(value => value.id !== id);
         setCars([...filterCarsArray]);
     };
 
     return (
         <div className={'cars-box'}>
-                <div className={'list'}> Car list  </div>
+            <div className={'list'}> Car list</div>
             {
                 cars.map(value => <Car key={value.id}
-                    item={value}
-                deleteCar={deleteCar}/>)
+                                       item={value}
+                                       deleteCar={deleteCar}/>)
             }
         </div>
     );
