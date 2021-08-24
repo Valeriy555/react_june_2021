20f3b63869e1d7c374e42b431c6b89f34190e832
Valery555 <sokolchenkov@gmail.com>
2021-08-24 19:19:25 +0300

    commit 24/08

---
+++ src/components/users/Company.js
@@ -0,0 +1,9 @@
+export default function Company({company: {name, catchPhrase, bs}}) {
+    return (
+        <div>
+            <b>Company: </b>
+            <br/> Name: {name}, CatchPhrase: {catchPhrase}, CatchPhrase:{catchPhrase};
+
+        </div>
+    );
+}
